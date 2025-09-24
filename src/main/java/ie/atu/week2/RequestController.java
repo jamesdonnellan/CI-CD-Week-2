31e9b37548a267492a586c15f894665fb3d6aeeb
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-09-24 15:24:36 +0100

    Created New Feature - UsingRequestParam

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,9 +1,6 @@
 package ie.atu.week2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -20,5 +17,12 @@ public class RequestController
     public String greetbyName(@PathVariable String name)
     {
         return "Hello " + name + "!";
+    }
+
+    //Using a new method to create an end point to display the users name and age
+    @GetMapping("/details/James/20")
+    public String details(@RequestParam String name, @RequestParam int age)
+    {
+        return "Name: " + name + ", Age: " + age;
     }
 }
